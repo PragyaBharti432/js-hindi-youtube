3e8e597dda26caacf1aa190769f9ff6595b6ecc8
PragyaBharti432 <156766977+PragyaBharti432@users.noreply.github.com>
2025-04-21 11:30:34 +0000

    commit done

---
+++ .devcontainer/basic_02/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "pragya@gmail.com"
+var accountPassword = "543211"
+let accountState;
+// accountId = 0986 this cant be done
+
+accountEmail= " sher@gmail.com"
+accountPassword="765"
+accountcity = "delhi"
+
+console.log(accountId);
+console.table([ accountId,accountEmail,accountcity , accountState])
+ //we should not use var because of issue in functional scope and block scope
